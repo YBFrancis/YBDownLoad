859ba6d22b8d47305161679ecd5db406b12a5cf1
yangbo <yangbo@yangbo-2.local>
2015-07-21 13:00:28 +0800

    初始化项目

---
+++ download/download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  download
+//
+//  Created by yangbo on 15/7/21.
+//  Copyright (c) 2015年 yangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ download/download/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  download
+//
+//  Created by yangbo on 15/7/21.
+//  Copyright (c) 2015年 yangbo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
