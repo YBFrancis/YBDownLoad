56956d33249f90402521c16c12c4c06a53ff198c
yangbo <yangbo@yangbo-2.local>
2015-07-21 13:03:57 +0800

    新建person类

---
+++ download/download/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  download
+//
+//  Created by yangbo on 15/7/21.
+//  Copyright (c) 2015年 yangbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ download/download/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  download
+//
+//  Created by yangbo on 15/7/21.
+//  Copyright (c) 2015年 yangbo. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
